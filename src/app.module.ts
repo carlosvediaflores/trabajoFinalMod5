092fe30f0dbc08bc8ac8dc7b151f33c4ca6f83f1
Carlos Vedia Flores <carlosvf78@gmail.com>
2024-09-13 23:55:53 -0400

    actividad 1 carlosvf78@gmail.com

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductosModule } from './productos/productos.module';
 
 @Module({
   imports: [ProductosModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
